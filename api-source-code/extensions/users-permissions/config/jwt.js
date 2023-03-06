5886bfb2d973bcd42e2cd9f22329d2eb399dc31b
Wayne <wayne@rainytownmedia.com>
2023-03-06 13:05:28 +0700

    no message

--- api-source-code/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '37a2e767-c1e4-42df-88cb-57098459b981'
-};
--- flutter-source-code/android/app/src/main/kotlin/com/envytheme/app/vaxim/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.rainytownmedia.app.shop
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
